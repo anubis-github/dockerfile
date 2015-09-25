e5c58422994c33c33d3f9191fbfb9b6d2011faf8
yurimelnikov <yuri.a.melnikov@gmail.com>
2015-09-26 01:50:55 +0300

    Sample idea project to run java program on docker created

---
+++ idea-settings/src/HelloFromDocker.java
@@ -0,0 +1,13 @@
+/**
+ * @author anubis
+ * @since 26/09/15
+ */
+public class HelloFromDocker {
+    public static void main(String[] args) throws Exception{
+        while (true){
+            System.out.println("Hello from Docker");
+
+            Thread.sleep(1000);
+        }
+    }
+}
